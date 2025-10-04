d6ef2f6d7abb72b1b2b16b4908a14bc04d558418
Ivan <mvzxw2@gmail.com>
2025-10-04 16:34:24 +0300

    init

--- index.js
+++ index.js
@@ -10,37 +10,37 @@ app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 app.use(cors())
 
-let isConnected = false
+// let isConnected = false
 
-async function connectToMongoDB() {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL)
-        isConnected = true
-        console.log("Connect to MongoDB")
-    } catch (err) {
-        console.error('Error connecting to MongoDB', err)
-    }
-}
+// async function connectToMongoDB() {
+//     try {
+//         await mongoose.connect(process.env.DATABASE_URL)
+//         isConnected = true
+//         console.log("Connect to MongoDB")
+//     } catch (err) {
+//         console.error('Error connecting to MongoDB', err)
+//     }
+// }
 
-app.use((req, res, next) => {
-    if (!isConnected) {
-        connectToMongoDB()
-    }
-    next()
-})
+// app.use((req, res, next) => {
+//     if (!isConnected) {
+//         connectToMongoDB()
+//     }
+//     next()
+// })
 
 app.use(router)
 
-// app.listen(process.env.PORT || 5000, async (err) => {
-//     if (err) {
-//         return console.log(err)
-//     }
+app.listen(process.env.PORT || 5000, async (err) => {
+    if (err) {
+        return console.log(err)
+    }
 
-//     mongoose.connect(process.env.DATABASE_URL)
-//     .then(() => console.log('DB ok'))
-//     .catch(err => console.log('DB error:', err))
+    mongoose.connect(process.env.DATABASE_URL)
+    .then(() => console.log('DB ok'))
+    .catch(err => console.log('DB error:', err))
 
-//     console.log('Server OK')
-// })
+    console.log('Server OK')
+})
 
-module.exports = app
+// module.exports = app
